c79a59c3ec9124074470a4a22dda4ba1bce908f6
beepritchard <sirin15@mac.com>
2020-03-28 14:40:34 -0500

    Updates

---
+++ precache-manifest.fc6b64b91696e0e37ea96255dcfbe2f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2613bade9e4e25d8146bd6e31ead75b4",
+    "url": "/clicky-space/index.html"
+  },
+  {
+    "revision": "04f1c791637e2ab42704",
+    "url": "/clicky-space/static/css/main.c1a16758.chunk.css"
+  },
+  {
+    "revision": "7a185ccc46f471b199de",
+    "url": "/clicky-space/static/js/2.b6fc60e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clicky-space/static/js/2.b6fc60e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f1c791637e2ab42704",
+    "url": "/clicky-space/static/js/main.e8ef146f.chunk.js"
+  },
+  {
+    "revision": "75740e58a1a3164b92bc",
+    "url": "/clicky-space/static/js/runtime-main.e14b93b4.js"
+  }
+]);
